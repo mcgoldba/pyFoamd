b3f45b08568d4fc365547b797baa00dcbdd3f663
Marc Goldbach <mcgoldba@gmail.com>
2021-07-26 16:52:37 -0400

    updated scripts

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     url='https://github.com/mcgoldba/pyFoamd',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
+    python_requires='>=3.7',
     install_requires=[
         'pint',
         'pandas'
