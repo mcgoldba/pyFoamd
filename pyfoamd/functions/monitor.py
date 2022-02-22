2d5bae8d8ed22d84070f1955053a1a3bfcd2e4dc
Marc Goldbach <mcgoldba@gmail.com>
2022-02-22 10:16:34 -0500

    imporovements to pfmonitor

--- pyfoamd/functions/monitor.py
+++ pyfoamd/functions/monitor.py
@@ -9,7 +9,9 @@ log = logging.getLogger("pf")
 def monitor(time=None, supplement=None, workingDir=os.getcwd()):
 
     commands = ['pfmonitor']
-    if time: commands.append(['-t', str(time)])
+    if time:
+        commands.append('-t')
+        commands.append(str(time))
     if supplement:
         commands.append('-s')
         for s in supplement:
@@ -20,3 +22,4 @@ def monitor(time=None, supplement=None, workingDir=os.getcwd()):
         subprocess.run(commands, cwd=workingDir)
     except KeyboardInterrupt:
         sys.exit(0)
+    # subprocess.call(commands, cwd=workingDir)
