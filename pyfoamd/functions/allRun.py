749d44be293f9ab87634b9edb1aa3a3ddb3c73fe
Marc Goldbach <mcgoldba@gmail.com>
2022-04-23 15:47:08 -0400

    log info

--- pyfoamd/functions/allRun.py
+++ pyfoamd/functions/allRun.py
@@ -8,4 +8,5 @@ def allRun(caseDir=Path.cwd()):
     if not isCase(caseDir):
         userMsg(f"Invalid case directory specified:\n {caseDir}", "ERROR")
     script_ = str(Path(caseDir) / 'Allrun')
+    userMsg(f"Running case {caseDir}")
     subprocess.call('./'+script_)
