32e722af7fcd733a568fab9400cfe24c1286f4d8
Marc <mcgoldba@gmail.com>
2022-06-03 15:03:57 -0400

    read times from decomposed cases.

--- pyfoamd/functions/getLatestTime.py
+++ pyfoamd/functions/getLatestTime.py
@@ -1,15 +1,45 @@
 import os
 from pathlib import Path
 
-def getLatestTime(directory=Path.cwd()):
+import logging
 
-    #- Get the latest time directory
-    directories = [f.name for f in Path(directory).iterdir()]
+# def getLatestTime(directory=Path.cwd()):
+
+#     #- Get the latest time directory
+#     directories = [f.name for f in Path(directory).iterdir()]
+#     latestTime = '0'
+#     for directory in directories:
+#         name = directory.replace('/', '')
+#         if name.isdigit() is True:
+#             if int(name) > int(latestTime):
+#                 latestTime = name
+
+#     return latestTime
+
+def getLatestTime(caseDir=Path.cwd()):
+
+    logger = logging.getLogger('xcfdv')
+    
+    #- Get the latest time directory for reconstructed case
+    directories = [f.name for f in os.scandir(Path(caseDir).resolve()) if f.is_dir()]
     latestTime = '0'
     for directory in directories:
         name = directory.replace('/', '')
         if name.isdigit() is True:
             if int(name) > int(latestTime):
                 latestTime = name
+
+    #- Get the latest time for the decomposed case
+    p0 = Path(caseDir) / 'processor0'
+    platestTime = '0'
+    if (p0).is_dir():
+        directories = [f.name for f in os.scandir(p0) if f.is_dir()]
+        for directory in directories:
+            name = directory.replace('/', '')
+            if name.isdigit() is True:
+                if int(name) > int(platestTime):
+                    platestTime = name
+
+    latestTime = max(float(latestTime), float(platestTime)) 
 
     return latestTime
